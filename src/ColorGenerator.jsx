2fa66a1493335aea26a2950438f2da55899a5bf6
Ahmad Hassan <ahassandevx@gmail.com>
2025-12-02 23:46:23 +0500

    changes

--- src/ColorGenerator.jsx
+++ src/ColorGenerator.jsx
@@ -12,7 +12,7 @@ const randomColor  =  '#' + Math.floor(Math.random()*16777215).toString(16);
     <div className='h-screen flex flex-col items-center justify-center'  style={{ backgroundColor: color }}>
         <h1 className="text-3xl font-bold mb-4 text-white">Random Color Generator</h1>
         <p className="text-white mb-4 text-xl">{color}</p>
-        <button onClick={generateColor} className="px-6 py-2 bg-white text-black rounded-lg shadow hover:bg-gray-200 transition">Generate Color</button>
+        <button onClick={generateColor} className="px-6 py-2 bg-white text-black rounded-lg shadow hover:bg-gray-200 transition cursor-pointer">Generate Color</button>
       
     </div>
   )
